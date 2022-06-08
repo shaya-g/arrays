d3e65f3ff5d30ece415dee339093d45862b4fd00
Shaya Golafshani <s.golaff@gmail.com>
2022-06-08 11:50:01 -0600

    Add Array methods

--- arrays.js
+++ arrays.js
@@ -7,3 +7,10 @@ var friendsPaid = [paidRent, notPaid, paidRent, notPaid]
 console.log(friends)
 console.log(rent)
 console.log(friendsPaid)
+
+//"Jill" will be removed from the friends array because it is the last element.
+friends.pop()
+//1500 will be removed from the rent array because it is the first element.
+rent.shift()
+//paidRent will be added to the end of the friendsPaid array.
+friendsPaid.push(paidRent)
