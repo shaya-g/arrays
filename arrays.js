95251372846ac06421710263f42e048c6ef9af65
Shaya Golafshani <s.golaff@gmail.com>
2022-06-08 11:39:44 -0600

    Add the arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var friends = ["Sally", "Bob", "Tim", "Jill"]
+var rent = [1500, 800, 2000, 1000]
+var paidRent = "true"
+var notPaid = "false"
+var friendsPaid = [paidRent, notPaid, paidRent, notPaid]
+
+console.log(friends)
+console.log(rent)
+console.log(friendsPaid)
